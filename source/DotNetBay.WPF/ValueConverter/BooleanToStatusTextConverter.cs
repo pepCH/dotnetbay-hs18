ea0b94f8f44df1584ff386e07375aff8eeb045db
Kamber Pascal (s) <pascal.kamber@students.fhnw.ch>
2018-10-30 12:02:45 +0100

    changs

---
+++ source/DotNetBay.WPF/ValueConverter/BooleanToStatusTextConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DotNetBay.WPF.ValueConverter
+{
+    public class BooleanToStatusTextConverter : IValueConverter
+    {
+        //backend -> ui
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool status = (bool)value;
+
+            string statusTxt = status ? "offen" : "geschlossen";
+
+            return statusTxt;
+        }
+
+        //from ui -> backend
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string statusTxt = (string)value;
+
+            bool status = "offen".Equals(statusTxt) ? true : false;
+
+            return status;
+        }
+    }
+}
+
